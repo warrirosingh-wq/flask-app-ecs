490d2aec804462dd789b3ceac0c8f303d9124dcf
Singh <warrirosingh@gmail.com>
2026-05-11 12:43:50 +0530

    Add tests for home and health routes

--- app.py
+++ app.py
@@ -8,3 +8,12 @@ def hello_world():
 @app.route('/health')
 def health():
     return 'Server is up and running'
+def test_home():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+
+def test_health():
+    client = app.test_client()
+    response = client.get('/health')
+    assert response.status_code == 200
